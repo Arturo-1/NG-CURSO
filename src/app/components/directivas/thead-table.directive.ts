1418873be8149d806a9a9c3eff115e45164ec67d
arturo <arturo.1231.f@gmail.com>
2022-12-20 12:29:17 -0600

    se complementa abm y se agregan pipe y directiva

--- src/app/components/directivas/thead-table.directive.ts
+++ src/app/components/directivas/thead-table.directive.ts
@@ -6,11 +6,11 @@ import { Directive, ElementRef, Renderer2, OnInit } from '@angular/core';
 export class TheadTableDirective implements OnInit {
 
   constructor(private elemento: ElementRef, private renderer:Renderer2) {
-    elemento.nativeElement.style.fontsize = '20px';
+    
   }
 
   ngOnInit(): void {
-    this.renderer.setStyle(this.elemento.nativeElement,'font-size','20px')
+    this.renderer.setStyle(this.elemento.nativeElement,'font-size','20px');
   }
 
 }
